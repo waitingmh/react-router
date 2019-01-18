787ca86302af46cd5073c8e05b1c67d4ae367091
mahh <1021681022@qq.com>
2019-01-18 21:14:23 +0800

    router

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './common/scss/index.scss';
+import App from './containers/app';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
